4b495c972e32a8304539501e751171bc61953b87
ARNAUDPORTABLE\Arnaud <arnaud.sadaca@eleves.enpc.fr>
2020-05-11 15:07:12 +0200

    VSwapHestonAnalatycalPricer modif

--- VarianceSwapsHestonAnalyticalPricer.cpp
+++ VarianceSwapsHestonAnalyticalPricer.cpp
@@ -1,4 +1,5 @@
 #include "VarianceSwapsHestonAnalyticalPricer.h"
+#include <complex>
 
 VarianceSwapsHestonAnalyticalPricer::VarianceSwapsHestonAnalyticalPricer(
                                             const HestonModel& hestonModel):
@@ -10,6 +11,36 @@ VarianceSwapsHestonAnalyticalPricer::VarianceSwapsHestonAnalyticalPricer(
 VarianceSwapsHestonAnalyticalPricer::~VarianceSwapsHestonAnalyticalPricer()
 {
 
+}
+
+complex<double> VarianceSwapsHestonAnalyticalPricer::
+a_term(const double omega) const {
+    complex<double> j(0.,1.);
+    return hestonModel_->getMeanReversionSpeed() - hestonModel_->getCorrelation() * hestonModel_->getVolOfVol() * omega * j  ;
+}
+
+complex<double> VarianceSwapsHestonAnalyticalPricer::
+b_term(const double omega) const {
+    complex<double> j(0.,1.);
+    return sqrt(a_term(omega)+ hestonModel_->getVolOfVol() * hestonModel_->getVolOfVol() * (j*omega + omega*omega) )  ;
+}
+
+complex<double> VarianceSwapsHestonAnalyticalPricer::
+g_term(const double omega) const {
+    return (a_term(omega)-b_term(omega))/(a_term(omega)+b_term(omega));
+}
+
+complex<double> VarianceSwapsHestonAnalyticalPricer::
+Function_C(const double tau, const double omega) const {
+    complex<double> j(0.,1.);
+    return tau * hestonModel_->getDrift() * (j * omega - 1.)+ hestonModel_->getMeanReversionSpeed() *  hestonModel_->getMeanReversionLevel() / (hestonModel_->getVolOfVol()*hestonModel_->getVolOfVol()) * ((a_term(omega)-b_term(omega))*tau-2.*log((1.-g_term(omega)*exp(-b_term(omega)*tau))/(1.-g_term(omega)))) ;
+}
+
+complex<double> VarianceSwapsHestonAnalyticalPricer::
+Function_D(const double tau, const double omega) const {
+    complex<double> D_init;
+    D_init=(a_term(omega)-b_term(omega))/(hestonModel_->getVolOfVol()*hestonModel_->getVolOfVol());
+    return D_init*(a_term(omega)+b_term(omega))*(1.-exp(-b_term(omega)*tau))/(1.-g_term(omega)*exp(-b_term(omega)*tau));
 }
 
 double VarianceSwapsHestonAnalyticalPricer::price(const VarianceSwap& varianceSwap) const{
