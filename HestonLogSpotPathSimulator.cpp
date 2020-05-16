092b423ee6233ef1925f91b0fa480c17db3da3a7
ZENG <claire.s.zeng@gmail.com>
2020-05-16 16:09:55 +0200

    LogSpotDiscretizationScheme

--- HestonLogSpotPathSimulator.cpp
+++ HestonLogSpotPathSimulator.cpp
@@ -43,3 +43,45 @@ double BroadieKayaScheme::nextStep(std::size_t currentIndex, double currentValue
 {
     //A compléter
 }
+
+
+void DiscretizationScheme::preComputations()
+{
+    double rho = hestonModel_->getCorrelation();
+    double theta = hestonModel_->getMeanReversionLevel();
+    double kappa = hestonModel_->getMeanReversionSpeed();
+    double eps = hestonModel_->getVolOfVol();
+    double delta;
+    for(std::size_t i = 0; i < timePoints_.size()-1; i++)
+    {
+        delta = timePoints_[i+1] - timePoints_[i];
+        k0_.push_back(-rho*kappa*theta*delta/eps);
+        k1_.push_back(gamma1_*delta*(kappa*rho/eps-0.5)-rho/eps);
+        k2_.push_back(gamma2_*delta*(kappa*rho/eps-0.5)+rho/eps);
+        k3_.push_back(gamma1_*delta*(1.-rho*rho));
+        k4_.push_back(gamma2_*delta*(1.-rho*rho));
+    }
+}
+
+void DiscretizationScheme::setGamma(double gamma1, double gamma2){
+    gamma1_ = gamma1;
+    gamma2_ = gamma2;
+}
+
+DiscretizationScheme::DiscretizationScheme(const DiscretizationScheme& broadieKayaScheme):
+        HestonLogSpotPathSimulator(broadieKayaScheme.timePoints_,
+                                    *broadieKayaScheme.hestonModel_,
+                                    *broadieKayaScheme.variancePathSimulator_)
+{
+
+}
+
+DiscretizationScheme* DiscretizationScheme::clone() const{
+    return new DiscretizationScheme(*this);
+}
+
+double DiscretizationScheme::nextStep(std::size_t currentIndex, double currentValue) const
+{
+    //A compléter
+}
+
--- HestonLogSpotPathSimulator.h
+++ HestonLogSpotPathSimulator.h
@@ -3,18 +3,18 @@
 
 #include "PathSimulator.h"
 #include "HestonVariancePathSimulator.h"
-//c'est galère d'écrire des commentaires en c++, non pas du tout
+
 class HestonLogSpotPathSimulator : public PathSimulator
 {
 protected:
     const HestonModel* hestonModel_;
     const HestonVariancePathSimulator* variancePathSimulator_;
 
-    virtual double nextStep(std::size_t currentIndex, double currentValue) const = 0; 
+    virtual double nextStep(std::size_t currentIndex, double currentValue) const = 0;
 public:
     HestonLogSpotPathSimulator(const std::vector<double>& timePoints,
-                                const HestonModel& hestonModel,
-                                const HestonVariancePathSimulator& variancePathSimulator);
+                               const HestonModel& hestonModel,
+                               const HestonVariancePathSimulator& variancePathSimulator);
     virtual ~HestonLogSpotPathSimulator();
     virtual HestonLogSpotPathSimulator* clone() const =0;
 };
@@ -24,13 +24,34 @@ private:
     double nextStep(std::size_t currentIndex, double currentValue) const;
 public:
     BroadieKayaScheme(const std::vector<double>& timePoints,
-                    const HestonModel& hestonModel,
-                    const HestonVariancePathSimulator& variancePathSimulator);
+                      const HestonModel& hestonModel,
+                      const HestonVariancePathSimulator& variancePathSimulator);
     BroadieKayaScheme(const BroadieKayaScheme& broadieKayaScheme);
     BroadieKayaScheme* clone() const;
 };
 
-/* Optional */ 
+class DiscretizationScheme : public HestonLogSpotPathSimulator
+{
+private:
+    double nextStep(std::size_t currentIndex, double currentValue) const;
+    double gamma1_;
+    double gamma2_;
+
+    std::vector<double>  k0_;
+    std::vector<double>  k1_;
+    std::vector<double>  k2_;
+    std::vector<double>  k3_;
+    std::vector<double>  k4_;
+    void preComputations();
+public:
+    DiscretizationScheme(const std::vector<double>& timePoints,
+                         const HestonModel& hestonModel,
+                         const HestonVariancePathSimulator& variancePathSimulator);
+    DiscretizationScheme(const DiscretizationScheme& discretizationScheme);
+    DiscretizationScheme* clone() const;
+};
+
+/* Optional */
 
 // class EulerScheme : public HestonLogSpotPathSimulator{
 // private:
